0b03d74b121367a3e580c1b9bea2cc75db81b7af
AekJ4s <Jasdakornam@gmail.com>
2024-02-05 09:43:35 +0700

    Update File App.js and Menu.js

--- quiz-app/src/component/Menu.js
+++ quiz-app/src/component/Menu.js
@@ -1,7 +1,11 @@
+import { useContext } from "react";
+import { DataContext } from "../App";
 const Menu = ()=>{
+    const {setAppState} = useContext(DataContext)
     return(
         <div className="quiz">
             <h1>Menu Component</h1>
+            <button onClick={()=>setAppState("quiz")}>เริ่มทำแบบข้อสอบ</button>
         </div>
     )
 }
