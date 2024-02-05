01718b05978b35d9b2c79a3280dc9fe7ba86cf95
AekJ4s <Jasdakornam@gmail.com>
2024-02-05 10:09:35 +0700

    Update Quiz.js
    สร้างผลลัพธ์หลังจากกดปุ่มแล้วจะเกิดผลอะไรตามมา

--- quiz-app/src/component/Quiz.js
+++ quiz-app/src/component/Quiz.js
@@ -1,18 +1,32 @@
 import QuestionsData from "../data/QuestionsData";
-import {useState} from "react"
-const Quiz = ()=>{
+import { useEffect,useState } from "react"
+const Quiz = () => {
     //console.log(QuestionsData);
-    const[current,setCurrent] =useState(0)
-    const[selectChoice,setSelectChoice] = useState("")
-    return(
-        
+    const [current, setCurrent] = useState(0)
+    const [selectChoice, setSelectChoice] = useState("")
+
+    useEffect(() => {
+        checkAnswer()
+    }, [selectChoice])
+
+    const checkAnswer = () => {
+        if (selectChoice !== "") {
+            if (selectChoice === QuestionsData[current].answer) {
+                console.log("ตอบถูกและได้คะแนน")
+            } else {
+                console.log("ตอบผิดจะได้คะแนนได้ไง งง")
+            }
+        }
+    }
+    return (
+
         <div className="quiz">
             <h1>{QuestionsData[current].question}</h1>
             <div className="chices">
-                <button onClick={()=>setSelectChoice("A")}>{QuestionsData[current].A}</button>
-                <button onClick={()=>setSelectChoice("B")}>{QuestionsData[current].B}</button>
-                <button onClick={()=>setSelectChoice("C")}>{QuestionsData[current].C}</button>
-                <button onClick={()=>setSelectChoice("D")}>{QuestionsData[current].D}</button>
+                <button onClick={() => setSelectChoice("A")}>{QuestionsData[current].A}</button>
+                <button onClick={() => setSelectChoice("B")}>{QuestionsData[current].B}</button>
+                <button onClick={() => setSelectChoice("C")}>{QuestionsData[current].C}</button>
+                <button onClick={() => setSelectChoice("D")}>{QuestionsData[current].D}</button>
             </div>
             <p>{`${current} / ${QuestionsData.length}`}</p>
         </div>
