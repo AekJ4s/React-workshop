35ce598230d5288753f5fe603eff18dc6f387e63
AekJ4s <Jasdakornam@gmail.com>
2024-02-05 09:25:38 +0700

    Create Quiz Menu and Score

---
+++ quiz-app/src/component/Menu.js
@@ -0,0 +1,9 @@
+const Menu = ()=>{
+    return(
+        <div className="quiz">
+            <h1>Menu Component</h1>
+        </div>
+    )
+}
+
+export default Menu;
---
+++ quiz-app/src/component/Score.js
@@ -0,0 +1,9 @@
+const Score = ()=>{
+    return(
+        <div className="quiz">
+            <h1>Score Component</h1>
+        </div>
+    )
+}
+
+export default Score;
