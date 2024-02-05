5edb6fa115c231c9bfd680a04d58d5f110e20949
AekJ4s <Jasdakornam@gmail.com>
2024-02-05 09:31:45 +0700

    Update App.js

--- quiz-app/src/App.js
+++ quiz-app/src/App.js
@@ -1,10 +1,17 @@
-import logo from './logo.svg';
+import {useState} from "react"
 import './App.css';
+import Menu from "./component/Menu";
+import Quiz from "./component/Quiz";
+import Score from "./component/Score";
 
 function App() {
+  const [appState,setAppState] = useState("menu");
   return (
     <div className="App">
       <h1>Web Development Quiz</h1>
+      {appState === "menu" && <Menu/>}
+      {appState === "quiz" && <Quiz/>}
+      {appState === "score" && <Score/>}
     </div>
   );
 }
