268729d1e6e36748d6a1331b6cdd73c8f310de0c
AekJ4s <Jasdakornam@gmail.com>
2024-02-05 10:33:39 +0700

    Update Score.js
    เพิ่มปุ่มเพื่อทำแบบทดสอบใหม่

--- quiz-app/src/component/Score.js
+++ quiz-app/src/component/Score.js
@@ -2,12 +2,19 @@ import QuestionsData from "../data/QuestionsData";
 import { useContext } from "react";
 import { DataContext } from "../App";
 
-const Score = ()=>{
-    const {score} = useContext(DataContext)
-    return(
+const Score = () => {
+    const { score,setAppState,setScore } = useContext(DataContext)
+
+    const restartApp = () => {
+        setScore(0)
+        setAppState("menu")
+    }
+
+    return (
         <div className="score">
             <h1>Score Component</h1>
             <h2>{score} / {QuestionsData.length} </h2>
+            <button onClick={restartApp}>ทำการทดสอบใหม่อีกครั้ง</button>
         </div>
     )
 }
